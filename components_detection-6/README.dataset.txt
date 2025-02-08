83b2bcd490701e68e5677c266066d2f9e98693de
thesisgr4 <thesisgroupfourc@gmail.com>
2025-02-08 10:21:04 +0800

    Initial commit

---
+++ components_detection-6/README.dataset.txt
@@ -0,0 +1,6 @@
+# components_detection > 2025-02-07 6:36pm
+https://universe.roboflow.com/kyle-apiyh/components_detection
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- roboflowTrain.py
+++ roboflowTrain.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="ztuSjmkiccPdbrivPKxW")
 project = rf.workspace("kyle-apiyh").project("components_detection")
-version = project.version(5)
+version = project.version(6)
 dataset = version.download("yolov8")
