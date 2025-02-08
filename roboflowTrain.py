0db4162514e09070fb672c798e8083c72a0d536a
thesisgr4 <thesisgroupfourc@gmail.com>
2025-02-08 10:18:11 +0800

    Delete roboflowTrain.py

--- roboflowTrain.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="ztuSjmkiccPdbrivPKxW")
-project = rf.workspace("kyle-apiyh").project("components_detection")
-version = project.version(5)
-dataset = version.download("yolov8")
